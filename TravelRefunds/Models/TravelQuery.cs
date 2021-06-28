326d69ffe73da4f97b83fbfe4fa8e6c82f6104b2
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2021-06-28 17:24:56 +0200

    Add query parameters.

--- TravelRefunds/Models/TravelQuery.cs
+++ TravelRefunds/Models/TravelQuery.cs
@@ -1,16 +1,41 @@
-﻿using System;
-
-namespace TravelRefunds.Models
+﻿namespace TravelRefunds.Models
 {
+    using System;
+
+    /// <summary>
+    /// Travel query with locations and distance.
+    /// </summary>
     public class TravelQuery
     {
+        /// <summary>
+        /// From location, where user start to travel.
+        /// </summary>
         public string From { get; set; }
+
+        /// <summary>
+        /// To location, where user finish to travel.
+        /// </summary>
         public string To { get; set; }
+
+        /// <summary>
+        /// Distance Unit of the query. This value may change, accordingly to the distance.
+        /// </summary>
         public DistanceUnit DistanceUnit { get; set; }
+
+        /// <summary>
+        /// Travel distance of the query. This value may change, accordingly to the distance.
+        /// </summary>
         public double? Distance { get; set; }
+
+        /// <summary>
+        /// The Datetime when user has request the query.
+        /// </summary>
         public DateTime RequestTime { get; set; }
     };
 
+    /// <summary>
+    /// Distance Units, their number may increase.
+    /// </summary>
     public enum DistanceUnit
     {
         Kilometer
