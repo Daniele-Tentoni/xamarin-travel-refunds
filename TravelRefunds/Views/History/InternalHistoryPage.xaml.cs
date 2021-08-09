fa491570d12d3ac844fce6135ae22d8394f3b926
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2021-08-09 16:35:16 +0200

    More dark colors.

---
+++ TravelRefunds/Views/History/InternalHistoryPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelRefunds.Views.History
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InternalHistoryPage : ContentPage
+    {
+        public InternalHistoryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
